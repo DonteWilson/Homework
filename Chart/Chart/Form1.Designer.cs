e2a44d7e8d863d0b2991cc373238fcbfa48cc1ae
dwilsonaieyr1 <JittNA@aol.com>
2016-03-14 17:59:39 -0500

    commit
    3/14/16

---
+++ Chart/Chart/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace Chart
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.DamageMeter = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.DamageMeter)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // DamageMeter
+            // 
+            this.DamageMeter.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            chartArea1.Name = "ChartArea1";
+            this.DamageMeter.ChartAreas.Add(chartArea1);
+            this.DamageMeter.Cursor = System.Windows.Forms.Cursors.Hand;
+            legend1.Name = "Legend1";
+            this.DamageMeter.Legends.Add(legend1);
+            this.DamageMeter.Location = new System.Drawing.Point(89, 38);
+            this.DamageMeter.Name = "DamageMeter";
+            series1.BackGradientStyle = System.Windows.Forms.DataVisualization.Charting.GradientStyle.DiagonalRight;
+            series1.BackHatchStyle = System.Windows.Forms.DataVisualization.Charting.ChartHatchStyle.DiagonalBrick;
+            series1.BackImageTransparentColor = System.Drawing.Color.White;
+            series1.BorderColor = System.Drawing.Color.Coral;
+            series1.ChartArea = "ChartArea1";
+            series1.Color = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            series1.Legend = "Legend1";
+            series1.Name = "Damage Roll";
+            this.DamageMeter.Series.Add(series1);
+            this.DamageMeter.Size = new System.Drawing.Size(525, 354);
+            this.DamageMeter.TabIndex = 0;
+            this.DamageMeter.Text = "chart1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(89, 416);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Attack";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.attack_button_click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(179, 416);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Reset";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(740, 481);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.DamageMeter);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.DamageMeter)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart DamageMeter;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Chart/Chart/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chart
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+   
+        private void attack_button_click(object sender, EventArgs e)
+        {
+            Random rand = new Random();
+
+
+            List<int> DiceData = new List<int> { 3, 5, 6, 1, 6, 6, 4, 1, 2, 1, 1, 5 ,4 ,1 ,5, 1, 3, 2, 1 ,2 }; //List of Data for dice rolls
+            DiceData.Sort();
+
+            foreach (int i in DiceData)
+            {
+               DamageMeter.Series["Damage Roll"].Points.AddY(i);
+            }
+            
+            int total = DiceData.Count;
+            int sum = 0;
+            for (int i = 0; i < DiceData.Count; ++i)
+                sum += DiceData[i];
+            float mean = (float)sum / total;
+            float variance = 0;
+        }
+    }
+}
